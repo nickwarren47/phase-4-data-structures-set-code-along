414a3a9029fb1ba1aa210edad6a3697856976f34
Nick Warren <nickwarren47@gmail.com>
2022-10-24 19:45:32 -0600

    Done with assignment

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,54 @@
 # your code here
+
+
+# code from code along:
+class MySet 
+    def initialize(enumerable = []) 
+        @hash = {}
+        enumerable.each do |value| 
+            @hash[value] = true 
+        end
+    end 
+    def include?(value)
+        @hash.has_key?(value)
+    end
+    def add(value)
+        @hash[value] = true
+        self
+    end 
+    def delete(value)
+        @hash.delete(value)
+        self
+    end
+    def size 
+        @hash.size 
+    end
+end
+
+
+# set = MySet.new([1,2,3,3])
+
+# Code refactored (using attr_reader): 
+class MySet 
+    attr_reader :hash
+    def initialize(enumerable = []) 
+        @hash = {}
+        enumerable.each do |value| 
+            add(value)
+        end
+    end 
+    def include?(value)
+        hash.has_key?(value)
+    end
+    def add(value)
+        hash[value] = true
+        self
+    end 
+    def delete(value)
+        hash.delete(value)
+        self
+    end
+    def size 
+        hash.size 
+    end
+end
